f95ccef15b50601188324d452eb91a724cd90d4f
ppymou <ppymou@gmail.com>
2012-12-01 20:09:38 -0500

    Basic Homepage done

--- app/index.js
+++ app/index.js
@@ -6,13 +6,17 @@ var redis = require('redis');
 var app = express();
 app.use(assets());
 app.use(express.static(process.cwd() + '/public'));
+app.use(express.static(process.cwd() + '/assets/img'));
 app.set('view engine', 'jade');
 
 var db = redis.createClient();
 
 app.get('/', function(req, resp) {
+  /*
   db.set('mykey', 'myvalue', function(err, val) {
   });
+  */
+
   return resp.render('index');
 });
 
@@ -21,6 +25,19 @@ app.get('/getval', function(req, resp) {
     console.log('getval: ' + val);
     return resp.send(val);
   });
+});
+
+app.get('/img/:filename', function(req, resp) {
+
+    var filename = req.params.filename;
+    res.sendfile();
+
+  /*
+  db.set('mykey', 'myvalue', function(err, val) {
+  });
+  */
+
+  return resp.render('index');
 });
 
 port = process.env.PORT || process.env.VMC_APP_PORT || 3000;
